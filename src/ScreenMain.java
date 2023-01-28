aba7c7a7c7b425e8e9fdf9e5dbdbdec59a928e50
yuval hadar <yuvalhad12@gmail.com>
2023-01-29 00:00:09 +0200

    added some notes in ScreenMain

--- src/ScreenMain.java
+++ src/ScreenMain.java
@@ -3,14 +3,18 @@ import java.awt.*;
 /*
 TODO LIST:
 
-1. add wall collision check
-2. add start menu
-3. show lives, score
-4. design a score icon
-5. implement ghosts
-6. save to db
-7. random levels
-8. multi player
+    1. add wall collision check
+    2. add start menu
+    3. show lives, score
+    4. design a score icon
+    5. implement ghosts
+    6. save to db
+    7. random levels
+    8. multi player
+
+http://www.camick.com/java/source/RXCardLayout.java
+
+
 
  */
 
