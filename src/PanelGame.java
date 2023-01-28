a1deec46f76fd9bd620c04472e7cb51b6383f05b
yuval hadar <yuvalhad12@gmail.com>
2023-01-28 14:59:08 +0200

    working on game panel

--- src/PanelGame.java
+++ src/PanelGame.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class PanelGame {
+public class PanelGame extends JPanel{
     PanelMap mapPanel;
     JPanel dataPanel;
 
     public PanelGame(){
+        dataPanel = new JPanel();
+        dataPanel.setLayout(new GridLayout(0,1));
+        JLabel livesLabel = new JLabel("Lives: 3");
+        JLabel scoreLabel = new JLabel("Score: 0");
+
+        dataPanel.add(livesLabel);
+        dataPanel.add(scoreLabel);
+
+
+        setLayout(new BorderLayout());
+        add(mapPanel, BorderLayout.CENTER);
+        add(dataPanel, BorderLayout.PAGE_END);
+
 
     }
 }
